ef2d22f162e22bdb89ab270c1b9bbf735ab203d7
Syed Fawzul Azim <syed.fawzul.azim@gmail.com>
2023-04-12 16:08:55 +0200

    feat: add app module

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { ApplicationModule } from './application/application.module';
+import { InfrastructureModule } from './infrastructure/infrastructure.module';
+import { DomainModule } from './domain/domain.module';
+@Module({
+  imports: [ApplicationModule, InfrastructureModule, DomainModule],
+  providers: [],
+  controllers: [],
+})
+export class AppModule {}
